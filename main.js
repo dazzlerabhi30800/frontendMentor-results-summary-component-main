21e786bb0e7d8e76010c9a83a4a40245c5e44f43
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-05-03 22:11:33 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => populateScore(json));
+
+const scoreWrapper = document.querySelector(".score--wrapper");
+const scoreElement = document.querySelector(".score");
+
+function populateScore(data) {
+  console.log(data);
+  scoreWrapper.innerHTML = data.map(
+    (item) =>
+      `
+        <div class="score--card ${item.category}--card">
+            <div class="score--logo">
+              <img src=${item.icon} alt="reaction" />
+              <p>${item.category}</p>
+            </div>
+            <p class="total--score"><span>${item.score}</span> / 100</p>
+          </div>
+        `
+  );
+}
+
+function setScore() {
+  let score = 0;
+  let interval = setInterval(() => {
+    score++;
+    if (score >= 76) {
+      score = 76;
+      clearInterval(interval);
+    }
+    scoreElement.innerText = score;
+  }, 30);
+}
+
+setScore();
